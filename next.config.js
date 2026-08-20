e7541b5882cbe20e07097133689f67e2f04bccd8
Nora-ML <86887904+Nora-ML@users.noreply.github.com>
2026-08-20 16:59:03 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+	images: {
+		domains: ["murjan-opti.s3.amazonaws.com", "murjan.s3.amazonaws.com"],
+	},
+	reactStrictMode: false,
+};
+//https://murjan.s3.amazonaws.com/Murjan_compress
